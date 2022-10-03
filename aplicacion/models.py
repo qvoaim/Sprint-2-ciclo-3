c3701ee55aba087332f402430dfcdd9aecd42d56
qvoaim <qvoaim@hotmail.com>
2022-10-03 07:33:21 -0500

    Update models.py

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Entidad(models.Model):
+    id=models.IntegerField(primary_key=True)
+    nombre=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=30)
+
 # Create your models here.

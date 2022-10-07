35562e70bd9bd67dca7c2046580a895758ad1b2d
qvoaim <qvoaim@hotmail.com>
2022-10-07 05:27:02 -0500

    Add files via upload

--- sprint/asgi.py
+++ sprint/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sprint project.
+ASGI config for Sprint project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sprint.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sprint.settings')
 
 application = get_asgi_application()

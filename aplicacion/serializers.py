d6fd4542b3a7ddb27a59ac6908b96ca8972600af
qvoaim <qvoaim@hotmail.com>
2022-10-07 05:53:51 -0500

    Delete aplicacion directory

--- aplicacion/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Entidad
-
-class EntidadSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Entidad
-        fields='_all_'
--- aplicacion/viewsets.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from . import models
-from . import  serializers
-
-class EntidadViewset(viewsets.ModelViewset):
-    queryset=models.Entidad.objects.all()
-    serializers_class=serializers.EntidadSerializer
-    

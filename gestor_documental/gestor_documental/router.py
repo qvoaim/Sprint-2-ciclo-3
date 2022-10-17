e23823033597ed016da6a9cbb90defc4bfc97e2a
Jhon Harold Pizarro <jpizarro@nexura.com>
2022-10-17 10:21:17 -0500

    Gestor Documental

--- gestor_documental/gestor_documental/router.py
+++
@@ -1,7 +0,0 @@
-from app.viewsets import UsuariosViewset
-from app.viewsets import LibrosViewset
-from rest_framework import routers
-
-router=routers.DefaultRouter()
-router.register('usuarios', UsuariosViewset)
-router.register('libros', LibrosViewset)
